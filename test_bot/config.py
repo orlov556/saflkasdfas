b01c43cbdd057e21418df89db667eed184e81634
orlov556 <orlovsacha08@gmail.com>
2026-07-19 19:26:34 +0300

    Update config.py

--- test_bot/config.py
+++ test_bot/config.py
@@ -1,4 +1,15 @@
-BOT_TOKEN = "8744501274:AAGzPYq2HL1GmTUOVRLnHe7IDdlnCymmi98"
+# config.py
+import os
 
-# Для теста используем SQLite (легко и быстро)
-DATABASE_URL = "sqlite+aiosqlite:///test_bot.db"
+BOT_TOKEN = os.getenv("BOT_TOKEN", "твой_токен_сюда")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("ADMIN_IDS") else []
+
+MIN_TICKETS = 5
+MAX_TICKETS = 100
+MAX_WINNERS = 100
+
+CB_PREFIX = "lot"
+CB_TICKET = f"{CB_PREFIX}:ticket"
+CB_ACCOUNT = f"{CB_PREFIX}:account"
+CB_WITHDRAW = f"{CB_PREFIX}:withdraw"
+CB_ADD_BOT = f"{CB_PREFIX}:addbot"
