4f71c7a179305e647af8fe07ef7db114a68c9ac5
orlov556 <orlovsacha08@gmail.com>
2026-07-19 19:09:04 +0300

    Update BOT_TOKEN in config.py

--- test_bot/config.py
+++ test_bot/config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "8259139768:AAHOMJLE74UGwbwiP-ggit3dxOL7N18tQ9M"
+BOT_TOKEN = "8744501274:AAGzPYq2HL1GmTUOVRLnHe7IDdlnCymmi98"
 
 # Для теста используем SQLite (легко и быстро)
 DATABASE_URL = "sqlite+aiosqlite:///test_bot.db"
